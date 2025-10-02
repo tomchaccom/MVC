8d6b3e7e870256feca1de8a89c05a4140751b065
tomchccom <dreamkms2014@gmail.com>
2025-10-02 18:28:25 +0900

    Remove IDE and system files from repository

--- backend/src/main/java/com/example/mvc/Article/Article.java
+++ backend/src/main/java/com/example/mvc/Article/Article.java
@@ -27,6 +27,7 @@
         private String content;
         private String author;
 
+        @Column(updatable = false)
         private LocalDateTime createdAt;
 
         @PrePersist // Entity 가 DB에 Insert(영속화) 되기 전에 실행되는 메소드를 의미, @preUpdate도 있음
@@ -34,6 +35,7 @@
             this.createdAt = LocalDateTime.now();
         }
         // @PerPersist를 사용하면 정확하게 DB에 저장되는 시점으로 createdAt이 저장, 객체의 생성 시점과는 다름
+
 
         public Article(String title, String content, String author) {
             this.title = title;
